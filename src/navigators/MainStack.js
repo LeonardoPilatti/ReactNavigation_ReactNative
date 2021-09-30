e7c7e71a984826865f37f8bf44ba4627d77cbc3f
Leonardo Pilatti <leonardopilatti14@gmail.com>
2021-09-29 21:49:12 -0300

    personalizando o header das paginas home e about

--- src/navigators/MainStack.js
+++ src/navigators/MainStack.js
@@ -8,9 +8,38 @@ const MainStack = createStackNavigator();
 
 export default () => {
   return (
-    <MainStack.Navigator>
-      <MainStack.Screen name="Home" component={HomeScreen} />
-      <MainStack.Screen name="About" component={AboutScreen} />
+    <MainStack.Navigator
+      screenOptions={{
+        /// esse screenOptions é para ele ficar com um valor padrão, caso tenha o options dentro do mainstack, então ele irá colocar esses options do mainstack;
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#ff0000',
+          height: 80,
+        },
+        headerTitleStyle: {
+          color: '#fff',
+          fontSize: 16,
+        },
+      }}>
+      <MainStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          //// essas options é para alterar o header;
+          title: 'Início',
+          headerTitleAlign: 'center',
+          headerStyle: {backgroundColor: '#6666ff', height: 75},
+          headerTitleStyle: {color: '#fff', fontSize: 23, fontWeight: 'bold'},
+        }}
+      />
+      <MainStack.Screen
+        name="About"
+        component={AboutScreen}
+        options={({route}) => ({
+          //// aqui estou pegando das props que foi para o about e colocando ele como titulo no header;
+          title: route.params?.name ?? 'Visitante',
+        })}
+      />
     </MainStack.Navigator>
   );
 };
